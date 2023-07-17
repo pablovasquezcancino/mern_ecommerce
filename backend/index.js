f31a07b3c5c53b2590eba996db99230c788a32fb
Pablo <pablovasquezcancino@gmail.com>
2023-07-17 12:37:39 -0400

    creacion routes, asynchandleres y conexion con db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express";
-import products from './products.js';
+
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import productRouter from "./routes/productRoutes.js";
 dotenv.config();
 
 
@@ -14,14 +15,8 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRouter)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);  
-})
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
