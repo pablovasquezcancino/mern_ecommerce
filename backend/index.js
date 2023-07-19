4a13b8a1dfe8a85f5238cc71fe5ca310ea3a80a1
Pablo <pablovasquezcancino@gmail.com>
2023-07-19 10:41:29 -0400

    product controller

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRouter from "./routes/productRoutes.js";
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 dotenv.config();
 
 
@@ -16,7 +18,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRouter)
-
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
